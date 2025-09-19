b83ec9047189f5b49a9869de8cdda09ea5bc69c7
Armand <armand.vieau@epita.fr>
2025-09-19 21:24:44 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+// select the toggle button
+const toggleBtn = document.querySelector(".header__toggle-btn")
+// select the header
+const header = document.querySelector(".header")
+
+toggleBtn.addEventListener("click", () => {
+	header.classList.toggle("header-open");
+	console.log("run the function here !!");
+})
